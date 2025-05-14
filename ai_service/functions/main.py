04829f1cb16dc902973b660b3f47d7af40ee6083
DenSandu <denisgabriel.sanduleanu@mail.polimi.it>
2025-05-14 12:40:41 +0200

    Added DTO and handled some logging edge cases

--- ai_service/functions/main.py
+++ ai_service/functions/main.py
@@ -5,12 +5,11 @@ from firebase_functions import https_fn
 from flask import Response
 
 from ai_setup.graph_logic import run_graph
-from ai_setup.llm_setup import initialize_llm
+
 
 import logging
 from vertexai.language_models import TextEmbeddingModel
-from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
+
 
 
 
@@ -38,37 +37,6 @@ def process_speech(request: https_fn.Request) -> Union[Response, tuple[Response,
         return https_fn.Response(json.dumps(error_payload), status=500, mimetype='application/json')
 
 
-# preparing habits in order to be correctly embedded
-def extract_habit_chunks(user_data: dict) -> list[str]:
-    chunks = []
-    for habit_name, habit in user_data.get("habits", {}).items():
-        goal = habit.get("goal", "")
-        desc = habit.get("description", "")
-        history = habit.get("history", {})
-        metrics_summary = []
-
-        for record in history:
-            timestamp = record.get("timestamp", "")
-            notes = record.get("notes", "")
-            metrics = ", ".join([f"{k}: {v}" for k, v in record.get("metrics", {}).items()])
-            summary = f"Date: {timestamp}, Metrics: {metrics}, Notes: {notes}"
-            metrics_summary.append(summary)
-
-        chunk = f"Habit: {habit_name}\nGoal: {goal}\nDescription: {desc}\nHistory:\n" + "\n".join(metrics_summary)
-        chunks.append(chunk)
-    return chunks
-
-def embed_chunks(chunks: list[str]) -> list[list[float]]:
-    model = TextEmbeddingModel.from_pretrained("text-embedding-004")
-    embeddings = model.get_embeddings(chunks)
-    return [e.values for e in embeddings]  # Extract float vectors
-
-def get_top_chunks(query: str, chunks: list[str], chunk_embeddings: list[list[float]], embed_model) -> list[str]:
-    query_embedding = np.array(embed_model.get_embeddings([query])[0].values).reshape(1, -1)
-    chunk_embeddings_array = np.array(chunk_embeddings)
-    similarities = cosine_similarity(query_embedding, chunk_embeddings_array).flatten()
-    top_indices = similarities.argsort()[::-1][:3]
-    return [chunks[i] for i in top_indices]
 
 @https_fn.on_request()
 def generate_report(request: https_fn.Request) -> Union[Response, tuple[Response, int]]:
